7c698c7c33ecbf3d425bebc0ba369144dc8e2493
Leonardo Meza <52134252+leonardomeza87@users.noreply.github.com>
2021-08-18 22:57:54 -0400

    Add the login styles

---
+++ components/LoginButton.jsx
@@ -0,0 +1,11 @@
+import s from "../styles/Login.module.scss";
+
+const LoginButton = ({ disabled, children }) => {
+  return (
+    <button className={s.actionButton} disabled={disabled}>
+      {children}
+    </button>
+  );
+};
+
+export default LoginButton;
